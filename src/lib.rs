51aa7582b76672010999cb6b62d2681297169265
Duncan Fairbanks <duncanfairbanks6@gmail.com>
2025-05-31 01:40:08 -0700

    add tokio::fs

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,12 @@
 #[cfg(test)]
 mod tests {
-    use futures_lite::{AsyncReadExt, AsyncWriteExt};
-    use futures_util::StreamExt;
-    use std::{
-        io::{Read, Write},
-        path::{Path, PathBuf},
-    };
+    use std::path::{Path, PathBuf};
 
     #[smol_potat::test]
     async fn async_fs_read_after_write_consistency() {
+        use futures_lite::{AsyncReadExt, AsyncWriteExt};
+        use futures_util::StreamExt;
+
         let tmpdir = tempfile::tempdir().unwrap();
         let (path_tx, path_rx) = async_channel::bounded(16);
         let writer_dir = tmpdir.path().to_owned();
@@ -38,9 +36,56 @@ mod tests {
         write_task.await;
         read_task.await;
     }
+    fn async_fs_create_clobber_options() -> async_fs::OpenOptions {
+        let mut opts = async_fs::OpenOptions::new();
+        opts.read(true).write(true).create(true);
+        opts
+    }
+
+    #[tokio::test]
+    async fn tokio_fs_read_after_write_consistency() {
+        use futures_util::StreamExt;
+        use tokio::io::{AsyncReadExt, AsyncWriteExt};
+
+        let tmpdir = tempfile::tempdir().unwrap();
+        let (path_tx, path_rx) = async_channel::bounded(16);
+        let writer_dir = tmpdir.path().to_owned();
+        let write_task = tokio::spawn(async move {
+            let writer_dir_ref = &writer_dir;
+            let path_tx_ref = &path_tx;
+            let bytes = b"hello".as_slice();
+            futures_util::stream::iter(0..40_000)
+                .for_each_concurrent(4, |i| async move {
+                    let path = writer_dir_ref.join(i.to_string());
+                    let mut f = tokio_fs_create_clobber_options().open(&path).await.unwrap();
+                    f.write_all(bytes).await.unwrap();
+                    f.sync_data().await.unwrap();
+                    path_tx_ref.send(path).await.unwrap();
+                })
+                .await;
+        });
+        let read_task = tokio::spawn(async move {
+            let bytes = b"hello".as_slice();
+            while let Ok(path) = path_rx.recv().await {
+                let mut f = tokio::fs::File::open(path).await.unwrap();
+                let mut buf = Vec::with_capacity(5);
+                f.read_to_end(&mut buf).await.unwrap();
+                assert_eq!(bytes, buf);
+            }
+        });
+        write_task.await.unwrap();
+        read_task.await.unwrap();
+    }
+    fn tokio_fs_create_clobber_options() -> tokio::fs::OpenOptions {
+        let mut opts = tokio::fs::OpenOptions::new();
+        opts.read(true).write(true).create(true);
+        opts
+    }
 
     #[test]
     fn std_fs_read_after_write_consistency() {
+        use std::io::Read;
+
         let tmpdir = tempfile::tempdir().unwrap();
         let (path_tx, path_rx) = async_channel::bounded(16);
         let writer_dir = tmpdir.path();
@@ -63,13 +108,14 @@ mod tests {
             });
         });
     }
-
     fn writer_thread(
         dir: &Path,
         start: usize,
         end: usize,
         path_tx: async_channel::Sender<PathBuf>,
     ) {
+        use std::io::Write;
+
         let bytes = b"hello".as_slice();
         for i in start..end {
             let path = dir.join(i.to_string());
@@ -82,12 +128,6 @@ mod tests {
 
     fn std_fs_create_clobber_options() -> std::fs::OpenOptions {
         let mut opts = std::fs::OpenOptions::new();
-        opts.read(true).write(true).create(true);
-        opts
-    }
-
-    fn async_fs_create_clobber_options() -> async_fs::OpenOptions {
-        let mut opts = async_fs::OpenOptions::new();
         opts.read(true).write(true).create(true);
         opts
     }
